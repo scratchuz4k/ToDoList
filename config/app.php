36a95b4ce8e702fc2da6a8ed78e2ce8375e18589
scratchuz4k <whiteknight96@hotmail.com>
2020-12-22 12:34:36 +0000

    redo

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://my.marcosferreira.site'),
 
     'asset_url' => env('ASSET_URL', null),
 
@@ -119,7 +119,7 @@ return [
     |
     */
 
-    'key' => env('APP_KEY'),
+    'key' => env('APP_KEY','t5cvxoxgbb3kyvpmgh11k618aa9bkdhc'),
 
     'cipher' => 'AES-256-CBC',
 
